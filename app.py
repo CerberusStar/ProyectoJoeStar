a3c905ae77b3d25d0de27972da922ba7c0144741
Kabel <abel_acc@outlook.com>
2020-07-24 19:17:51 -0600

    AMPL completamente incorporado
    Ya está hecho un form que controla la parte de AMPL para el trainer, si queda tiempo me encargaré de hacer un controlador de errores, pero no es algo que por el momento tendré en cuenta en el avance. -Abel

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, session
+from Controladora import Control
 
+codigoampl = Control()
 app = Flask(__name__)
+app.secret_key = "prueba"
 
 
 @app.route("/")
@@ -8,26 +11,41 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/beforeAMPL")
-def question():
+@app.route("/user")
+def user():
+    return render_template("user.html")
 
-    return render_template("index.html")
+
+@app.route("/trainer")
+def trainer():
+    return render_template("trainer.html")
 
 
-@app.route("/ampl", methods=["GET", "POST"])
+@app.route("/trainer/beforeAmpl", methods=["POST"])
+def update():
+    name = request.form.get("name")
+    var = int(request.form.get("var"))
+    restriction = int(request.form.get("restriction"))
+    session["name"] = name
+    session["var"] = var
+    session["restriction"] = restriction
+    return redirect("/trainer/ampl")
+
+
+@app.route("/trainer/ampl", methods=["GET", "POST"])
 def ampl():
     listaVariables = []
     listaPrecios = []
     listaRestricciones = []
-    numbers = [1, 2, 3, 4]
-    restriccion = [1, 2]
+    var = session["var"]
+    restriccion = session["restriction"]
     if request.method == "GET":
         # código que toma el id y lo imprime
         # form a la tabla y ponerle el method GET
         # Ocupar un input type hidden para el id
-        return render_template("amplForm.html", number=numbers, restriccion=restriccion)
+        return render_template("amplForm.html", var=var, restriccion=restriccion)
     else:
-        for i in numbers:
+        for i in range(var):
             variable = request.form[f"variable{i}"]
             precio = request.form[f"precio{i}"]
 
@@ -36,11 +54,14 @@ def ampl():
 
         print(listaVariables)
 
-        for j in restriccion:
+        for j in range(restriccion):
             restriccion = request.form[f"restriction{j}"]
             listaRestricciones.append(restriccion)
         print(listaRestricciones)
 
+        codigoampl.createArchive(
+            session["name"], listaVariables, listaPrecios, listaRestricciones
+        )
         return redirect("/")
 
 
